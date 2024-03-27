396e5fc99f01233eda5f25b93ccfabeb89760a9a
tirslo <t@pather.org>
2024-03-28 00:59:53 +1100

    feat: move to cf

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,10 +17,11 @@ const config = {
   organizationName,
   projectName,
   // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
+  url: `https://docs.airpipe.io`,
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
