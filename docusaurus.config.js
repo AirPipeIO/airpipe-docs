947e8199ec34ee845328451ef779acbf17dc0bf0
tirslo <t@pather.org>
2024-03-28 01:00:38 +1100

    remove ghpages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,8 +26,8 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  // onBrokenLinks: 'throw',
+  // onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
