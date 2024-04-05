3cf633df5fee8caece66fcb20bd42592a5c18b4c
tirslo <t@pather.org>
2024-04-06 02:13:36 +1100

    feat: update transforms and add examples

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
