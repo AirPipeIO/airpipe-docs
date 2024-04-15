263085accb3e694d995310327c62142222e71984
tirslo <t@pather.org>
2024-04-15 14:28:20 +0000

    feat: set url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   organizationName,
   projectName,
   // Set the production url of your site here
-  url: `https://dummy-ap.pages.dev/`,
+  url: `https://docs.airpipe.io/`,
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
