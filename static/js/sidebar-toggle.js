2accab597964ebc33315ad10ab709bf633537a08
Kav. Pather <kav.pather@gmail.com>
2024-05-07 02:30:33 +1000

    Update sidebar-toggle.js

--- static/js/sidebar-toggle.js
+++ static/js/sidebar-toggle.js
@@ -4,7 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
   
   // Check if the 'sidebar' parameter is present and set to 'full'
   if (urlParams.has('sidebar') && urlParams.get('sidebar') === 'full') {
-    const navbarElement = document.querySelector('.navbar__toggle');
-    navbarElement.click();
+    // Function to simulate clicking on the navbar toggle
+    const clickNavbarToggle = () => {
+      const navbarElement = document.querySelector('.navbar__toggle');
+      navbarElement.click();
+    };
+
+    const checkNavbarToggle = () => {
+      const navbarElement = document.querySelector('.navbar__toggle');
+      if (navbarElement) {
+        clickNavbarToggle(); 
+      } else {
+        setTimeout(checkNavbarToggle, 100);
+      }
+    };
+
+    checkNavbarToggle();
   }
 });
